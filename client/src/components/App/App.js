befec234d92ff81ba5ee0924ef011192ba9112a3
Alacritas <jhwheeler@gmail.com>
2017-04-26 21:27:17 +0300

    Import Hero

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Header from '../Header/Header';
+import Hero from '../Hero/Hero';
 
 const App = () => {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Hero backgroundImage="images/welcome-hero.jpg"/>
+    </div>
   );
 }
 
