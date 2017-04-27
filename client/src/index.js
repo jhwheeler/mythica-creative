e19bd78331a17f7042ecfe3db0fe0db9cf97fa0d
Alacritas <jhwheeler@gmail.com>
2017-04-27 22:03:25 +0300

    Change main div to 'root'; change css import to 'index.css'; change App component to Home

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import css from '../public/css/styles.css';
+import css from './index.css';
 
-import App from './components/App/App';
+import Home from './components/Home/Home';
 
 const Root = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={Home} />
       </Switch>
     </Router>
   )
 }
 
-render(<Root/>, document.querySelector("#main"));
+render(<Root/>, document.querySelector("#root"));
