c05d3c9ce94765ba7b29c947f7637e404a0815df
Alacritas <jhwheeler@gmail.com>
2017-05-03 21:53:04 +0300

    Fix formatting

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -5,9 +5,10 @@ import css from './Hero.css';
 export default class Hero extends React.Component {
   render() {
     const _style = {};
-        if (this.props.backgroundImage) {
-          _style.backgroundImage = `url(${this.props.backgroundImage})`;
-        }
+    if (this.props.backgroundImage) {
+      _style.backgroundImage = `url(${this.props.backgroundImage})`;
+    }
+
     return (
       <div className="hero" style={_style}>
         {this.props.children}
