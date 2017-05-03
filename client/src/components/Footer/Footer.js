c1b5eca4aeffe64af476706ad06ba0a85ae988e9
Alacritas <jhwheeler@gmail.com>
2017-05-03 21:54:12 +0300

    Create Footer component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import css from './Footer.css';
+
+const Footer = () => {
+  return(
+    <footer>
+      <span>
+        © 2017 Mythica Creative | Hand-crafted by Mythica Creative
+      </span>
+    </footer>
+  );
+}
+
+export default Footer;
