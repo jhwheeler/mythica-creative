abed00d0f7816db7317cffa2307d89fd730224b4
Alacritas <jhwheeler@gmail.com>
2017-05-03 21:56:51 +0300

    Import Header and Footer so they render on all routes

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,21 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import css from './index.css';
 
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 const Root = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
   )
 }
 
