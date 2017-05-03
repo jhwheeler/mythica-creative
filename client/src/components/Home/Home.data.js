d43b764a79edca9ec820a2d836218809073d9681
Alacritas <jhwheeler@gmail.com>
2017-05-03 21:56:21 +0300

    Move image imports to data file, Header and Footer to index.js

--- client/src/components/Home/Home.data.js
+++ client/src/components/Home/Home.data.js
@@ -1,4 +1,10 @@
-const copy = {
+import welcomeHero from '../../../public/images/hero/welcome-hero.jpg';
+import logoHero from '../../../public/images/hero/logo_hero.png';
+import mythica from '../../../public/images/hero/mythica.png';
+import creative from '../../../public/images/hero/creative.png';
+import globalReach from '../../../public/images/hero/global_reach_hero.jpg';
+
+export const copy = {
   mainSubHeader: "CREATE YOUR LEGEND",
   tagline1: "THE MOST SUCCESSFUL BRANDS LAST FOR GENERATIONS. OUR MISSION IS TO CREATE MORE THAN JUST A STORY.",
   tagline2: "WE ARE HERE TO CREATE YOUR LEGEND.",
@@ -6,4 +12,10 @@ const copy = {
   globalReach2: "From Europe to America to Asia, we help people of all backgrounds shape their future."
 }
 
-export default copy;
+export const images = {
+  welcomeHero,
+  logoHero,
+  mythica,
+  creative,
+  globalReach
+}
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
 import Services from '../Services/Services';
 import Projects from '../Projects/Projects';
@@ -9,27 +8,18 @@ import Founders from '../Founders/Founders';
 import CTA from '../CTA/CTA';
 
 import css from './Home.css';
-import copy from './Home.data.js';
-
-// Image Imports
-import welcomeHero from '../../../public/images/hero/welcome-hero.jpg';
-import logoHero from '../../../public/images/hero/logo_hero.png';
-import mythica from '../../../public/images/hero/mythica.png';
-import creative from '../../../public/images/hero/creative.png';
-import globalReach from '../../../public/images/hero/global_reach_hero.jpg';
+import { copy,images } from './Home.data.js';
 
 const Home = props => {
   return (
     <div className="home">
 
-      <Header />
-
       <div className="home-hero-wrapper">
-        <Hero backgroundImage={welcomeHero}>
+        <Hero backgroundImage={images.welcomeHero}>
           <div className="hero-content">
-            <img src={logoHero} className="home-hero-logo" />
-            <img src={mythica} className="home-hero-header" />
-            <img src={creative} className="home-hero-header-2" />
+            <img src={images.logoHero} className="home-hero-logo" />
+            <img src={images.mythica} className="home-hero-header" />
+            <img src={images.creative} className="home-hero-header-2" />
             <h3 className="home-hero-subheader">{copy.mainSubHeader}</h3>
           </div>
         </Hero>
@@ -43,7 +33,7 @@ const Home = props => {
       <Services />
 
       <div className="global-reach-hero">
-        <Hero backgroundImage={globalReach}>
+        <Hero backgroundImage={images.globalReach}>
           <div className="global-reach-hero-content hero-content">
             <h1>Global Reach</h1>
             <h2>{copy.globalReach1}</h2>
