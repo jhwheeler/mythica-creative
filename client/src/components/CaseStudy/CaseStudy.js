bab7801e00ed7c08c707d7e975629f2761139952
Alacritas <jhwheeler@gmail.com>
2017-05-06 17:28:01 +0300

    Remove this keyword before props

--- client/src/components/CaseStudy/CaseStudy.js
+++ client/src/components/CaseStudy/CaseStudy.js
@@ -5,18 +5,14 @@ import css from './CaseStudy.css';
 const CaseStudy = props => {
   return (
     <div className="case-study">
-      <img
-        className="main-image"
-        src={this.props.mainImage}
-        alt={this.props.mainImageName}
-      >
+      <img className="main-image" src={props.mainImage} alt={props.mainImageName} />
       <div className="case-study-text">
-        <h1>{this.props.header}</h1>
-        <h2>{this.props.subHeader}</h2>
-        <p>{this.props.summary}</p>
-        <p>Read More</p
+        <h1>{props.header}</h1>
+        <h2>{props.subHeader}</h2>
+        <p>{props.summary}</p>
+        <p>Read More</p>
         <div className="expanded-text">
-          {this.props.expandedText}
+          {props.expandedText}
         </div>
       </div>
     </div>
