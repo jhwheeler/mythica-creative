e7b36ae21d1b83bc4534d6f50a0eeedbbb699b6f
Alacritas <jhwheeler@gmail.com>
2017-05-09 02:52:40 +0300

    Move, rename, and import images for portfolio

--- client/src/components/Portfolio/Portfolio.data.js
+++ client/src/components/Portfolio/Portfolio.data.js
@@ -1,20 +1,233 @@
-import BusinessCardsAlhoff from '../../../public/images/portfolio/Business_Cards-Alhoff.jpg';
-import BusinessCardsFemanna from '../../../public/images/portfolio/Business_Cards-Femanna.jpg';
-import LogoGreenGlobal from '../../../public/images/portfolio/Logo_Green-Global-Solutions.jpg';
+/* Print */
+import AdFutureArt from '../../../public/images/portfolio/Ad_FutureArt.jpg';
+import AdFutureArt2 from '../../../public/images/portfolio/Ad_FutureArt2.jpg';
+import AdLivingHarmony from '../../../public/images/portfolio/Ad_LivingHarmony.jpg';
+import AdLivingHarmony2 from '../../../public/images/portfolio/Ad_LivingHarmony2.jpg';
+import AdWX1 from '../../../public/images/portfolio/Ad_WX1.jpg';
+import AdWX2 from '../../../public/images/portfolio/Ad_WX2.jpg';
+
+import BookDante from '../../../public/images/portfolio/Book_Dante.jpg';
+import BookMarquez from '../../../public/images/portfolio/Book_Marquez.jpg';
+
+import BrandMaterialsAlhoff from '../../../public/images/portfolio/BrandMaterials_Alhoff.jpg';
+import BrandMaterialsFlowAlchemy from '../../../public/images/portfolio/BrandMaterials_FlowAlchemy.jpg';
+import BrandMaterialsInfiniteLoveHealing from '../../../public/images/portfolio/BrandMaterials_InfiniteLoveHealing.jpg';
+import BrandMaterialsLivingHarmony from '../../../public/images/portfolio/BrandMaterials_LivingHarmony.jpg';
+import BrandMaterialsLivingHarmony2 from '../../../public/images/portfolio/BrandMaterials_LivingHarmony2.jpg';
+
+import BusinessCardsAlhoff from '../../../public/images/portfolio/BusinessCards_Alhoff.jpg';
+import BusinessCardsFemanna from '../../../public/images/portfolio/BusinessCards_Femanna.jpg';
+import BusinessCardsInfiniteLoveHealing from '../../../public/images/portfolio/BusinessCards_InfiniteLoveHealing.jpg';
+import BusinessCardsLivingHarmony from '../../../public/images/portfolio/BusinessCards_LivingHarmony.jpg';
+import BusinessCardsOnFire from '../../../public/images/portfolio/BusinessCards_OnFire.jpg';
+import BusinessCardsWX from '../../../public/images/portfolio/BusinessCards_WX.jpg';
+
+import PosterFutureArt from '../../../public/images/portfolio/Poster_FutureArt.jpg';
+import PosterLivingHarmony1 from '../../../public/images/portfolio/Poster_LivingHarmony1.jpg';
+import PosterLivingHarmony2 from '../../../public/images/portfolio/Poster_LivingHarmony2.jpg';
+import PosterLivingHarmony3 from '../../../public/images/portfolio/Poster_LivingHarmony3.jpg';
+
+import SignWX from '../../../public/images/portfolio/Sign_WX.jpg';
+import ProgramsFemanna from '../../../public/images/portfolio/Programs_Femanna.jpg';
+
+/* Logos */
+import LogoAlhoff from '../../../public/images/portfolio/Logo_Alhoff.jpg';
+import LogoAlhoff2 from '../../../public/images/portfolio/Logo_Alhoff2.jpg';
+import LogoAlhoffConcept from '../../../public/images/portfolio/Logo_Alhoff-Concept.jpg';
+import LogoConnect from '../../../public/images/portfolio/Logo_Connect.jpg';
+import LogoFemanna from '../../../public/images/portfolio/Logo_Femanna.jpg';
+import LogoFemannaConcept from '../../../public/images/portfolio/Logo_Femanna.jpg';
+import LogoFemannaLogotype from '../../../public/images/portfolio/Logo_Femanna-Logotype.jpg';
+import LogoFemannaMeaning from '../../../public/images/portfolio/Logo_Femanna-Meaning.jpg';
+import LogoFemannaVariations from '../../../public/images/portfolio/Logo_Femanna-Variations.jpg';
+import LogoFlowAlchemy from '../../../public/images/portfolio/Logo_FlowAlchemy.jpg';
+import LogoFutureArt from '../../../public/images/portfolio/Logo_FutureArt.jpg';
+import LogoFutureArtConcept from '../../../public/images/portfolio/Logo_FutureArt-Concept.jpg';
+import LogoFutureArtPictograms from '../../../public/images/portfolio/Logo_FutureArt-Pictograms.jpg';
+import LogoGreenGlobal from '../../../public/images/portfolio/Logo_GreenGlobal.jpg';
 import LogoHTA from '../../../public/images/portfolio/Logo_HTA.jpg';
+import LogoInfiniteLoveHealing from '../../../public/images/portfolio/Logo_InfiniteLoveHealing.jpg';
+import LogoInfiniteLoveHealingConcept from '../../../public/images/portfolio/Logo_InfiniteLoveHealing-Concept.jpg';
+import LogoInfiniteLoveHealingLogotype from '../../../public/images/portfolio/Logo_InfiniteLoveHealing-Logotype.jpg';
+import LogoLivingHarmony from '../../../public/images/portfolio/Logo_LivingHarmony.jpg';
+import LogoLivingHarmony2 from '../../../public/images/portfolio/Logo_LivingHarmony2.jpg';
+import LogoLivingHarmonyColors from '../../../public/images/portfolio/Logo_LivingHarmony-Colors.jpg';
+import LogoLivingHarmonyConcept from '../../../public/images/portfolio/Logo_LivingHarmony-Concept.jpg';
+import LogoOnfire from '../../../public/images/portfolio/Logo_OnFire.jpg';
+import LogoTaroLink from '../../../public/images/portfolio/Logo_TaroLink.jpg';
+import LogoWX from '../../../public/images/portfolio/Logo_WX.jpg';
+import LogoWX2 from '../../../public/images/portfolio/Logo_WX2.jpg';
+import LogoWX3 from '../../../public/images/portfolio/Logo_WX3.jpg';
+import LogoWXConcept from '../../../public/images/portfolio/Logo_WX-Concept.jpg';
+
+/* Web */
+import WebAlhoff from '../../../public/images/portfolio/Web_Alhoff.jpg';
+import WebAlhoff2 from '../../../public/images/portfolio/Web_Alhoff2.jpg';
+import WebDaraHelp from '../../../public/images/portfolio/Web_DaraHelp.jpg';
+import WebDaraHelp2 from '../../../public/images/portfolio/Web_DaraHelp2.jpg';
+import WebFemanna from '../../../public/images/portfolio/Web_Femanna.jpg';
+import WebFemanna2 from '../../../public/images/portfolio/Web_Femanna2.jpg';
+import WebFemanna3 from '../../../public/images/portfolio/Web_Femanna3.jpg';
+import WebFlowAlchemy from '../../../public/images/portfolio/Web_FlowAlchemy.jpg';
+import WebFlowAlchemy2 from '../../../public/images/portfolio/Web_FlowAlchemy2.jpg';
+import WebFlowAlchemy3 from '../../../public/images/portfolio/Web_FlowAlchemy3.jpg';
+import WebFlowAlchemy4 from '../../../public/images/portfolio/Web_FlowAlchemy4.jpg';
+import WebInfiniteLoveHealing from '../../../public/images/portfolio/Web_InfiniteLoveHealing.jpg';
+import WebInfiniteLoveHealing2 from '../../../public/images/portfolio/Web_InfiniteLoveHealing2.jpg';
+import WebInfiniteLoveHealing3 from '../../../public/images/portfolio/Web_InfiniteLoveHealing3.jpg';
+import WebLivingHarmony from '../../../public/images/portfolio/Web_LivingHarmony.jpg';
+import WebWX from '../../../public/images/portfolio/Web_WX.jpg';
+import WebWX2 from '../../../public/images/portfolio/Web_WX2.jpg';
+import WebWX3 from '../../../public/images/portfolio/Web_WX3.jpg';
 
 const categories = {
   logo: [
     {
-      name: 'Green Global',
+      name: 'Alhoff Logo',
+      src: LogoAlhoff
+    },
+    {
+      name: 'Alhoff Logo Variations',
+      src: LogoAlhoff2
+    },
+    {
+      name: 'Alhoff Logo Concept',
+      src: LogoAlhoffConcept
+    },
+    {
+      name: 'Connect Logo',
+      src: LogoConnect
+    },
+    {
+      name: 'Femanna Logo',
+      src: LogoFemanna
+    },
+    {
+      name: 'Femanna Logo Concept',
+      src: LogoFemannaConcept
+    },
+    {
+      name: 'Femanna Logotype',
+      src: LogoFemannaLogotype
+    },
+    {
+      name: 'Femanna Logo Meaning',
+      src: LogoFemannaMeaning
+    },
+    {
+      name: 'Femanna Logo Variations',
+      src: LogoFemannaVariations
+    },
+    {
+      name: 'Green Global Logo',
       src: LogoGreenGlobal
+    },
+    {
+      name: 'HTA Logo',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
+    },
+    {
+      name: 'HTA',
+      src: LogoHTA
     },
     {
       name: 'HTA',
       src: LogoHTA
     },
   ],
-  businessCards: [
+  print: [
     {
       name: 'Alhoff',
       src: BusinessCardsAlhoff
