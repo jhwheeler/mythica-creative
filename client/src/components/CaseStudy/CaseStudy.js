6b44614143dd9238d6fdfe94fe6e51133430c4fa
Alacritas <jhwheeler@gmail.com>
2017-05-10 22:52:41 +0300

    Add onclick handler to expand text for read more button

--- client/src/components/CaseStudy/CaseStudy.js
+++ client/src/components/CaseStudy/CaseStudy.js
@@ -3,29 +3,51 @@ import React from 'react';
 import css from './CaseStudy.css';
 import Button from '../Button/Button';
 
-const CaseStudy = props => {
-  return (
-    <div className="case-study">
-      <div className="main-image">
-        <img src={props.mainImage} alt={props.mainImageName} />
+export default class CaseStudy extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      readMore: false
+    }
+    this.expandText = this.expandText.bind(this);
+  }
+
+  expandText() {
+    event.preventDefault();
+    let readMore = this.state.readMore;
+    this.setState({
+      readMore: readMore === true ? false : true
+    });
+    console.log("hi");
+  }
+
+  render() {
+    let expandedText = null;
+    if (this.state.readMore === true) {
+      expandedText = <div dangerouslySetInnerHTML={{__html: this.props.expandedText}} className="expanded-text" />;
+    } else {
+      expandedText = null;
+    }
+    return (
+      <div className="case-study">
+        <div className="main-image">
+          <img src={this.props.mainImage} onClick={this.expandtext} alt={this.props.mainImageName} />
+        </div>
+        <div className="case-study-text">
+          <h1>{this.props.header}</h1>
+          <hr />
+          <p><strong>{this.props.subHeader}</strong></p>
+          <hr />
+          <p>{this.props.summary}</p>
+          <Button label="READ MORE" />
+          {expandedText}
+          {this.props.link !== undefined ?
+            <a href={this.props.link} target="_blank">
+              <Button label="SEE IT LIVE" />
+            </a>
+            : null}
+        </div>
       </div>
-      <div className="case-study-text">
-        <h1>{props.header}</h1>
-        <hr />
-        <p><strong>{props.subHeader}</strong></p>
-        <hr />
-        <p>{props.summary}</p>
-        <Button label="READ MORE" />
-        <div dangerouslySetInnerHTML={{__html: props.expandedText}} className="expanded-text" />
-        {props.link !== undefined ?
-          <a href={props.link} target="_blank">
-            <Button label="SEE IT LIVE" />
-          </a>
-          : null}
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default CaseStudy;
-
