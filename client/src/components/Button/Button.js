71bec4f2504d17118a99be8ebba8ed8f58c8b745
Alacritas <jhwheeler@gmail.com>
2017-05-10 22:52:09 +0300

    Change Button from input to button

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -5,7 +5,7 @@ import css from './Button.css';
 export default class Button extends React.Component {
   render() {
     return (
-      <input className="button" type="button" value={this.props.label} />
+      <button className="button">{this.props.label}</button>
     );
   }
 }
