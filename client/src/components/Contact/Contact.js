d568360d941eaadfdf77c07f12463d36c0b99cae
Alacritas <jhwheeler@gmail.com>
2017-05-12 12:43:42 +0300

    Build contact page

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
 import Hero from '../Hero/Hero';
-import CTA from '../CTA/CTA';
+import Button from '../Button/Button';
 
 import css from './Contact.css';
-import { copy,images } from './Contact.data.js';
+import contactHero from '../../../public/images/hero/ContactUs.jpg';
 
 const Contact = props => {
   return (
-    <h1>Contact</h1>
+    <div className="contact">
+      <Hero className="contact-hero" backgroundImage={contactHero}>
+        <div className="contact-form-wrapper">
+          <form class="contact-form">
+            <fieldset name="contact-info">
+              <label for="name" className="visually-hidden">Your Name</label>
+              <br/>
+              <input className="input" type="text" name="name" placeholder="Your Name" id="name" />
+              <label for="email" className="visually-hidden">Your Email</label>
+              <br/>
+              <input className="input" type="email" name="email" placeholder="Your Email" id="email" />
+              <br/>
+              <label for="message" className="visually-hidden">What's on your mind?</label>
+              <br/>
+              <textarea name="message" placeholder="What's on your mind?" id="message" />
+              <Button className="contact-button" label="START" />
+            </fieldset>
+          </form>
+        </div>
+      </Hero>
+    </div>
   );
 }
 
