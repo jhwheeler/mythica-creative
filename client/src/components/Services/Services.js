1c880e19aa453ac206f935f954ba2487d0975216
Alacritas <jhwheeler@gmail.com>
2017-05-16 16:36:07 +0300

    Fixed modal issue for Services
    Same as for the Projects component: isolating a Service component
    prevents all modals from being fired at once. Also merged openModal and
    closeModal into toggleModal.

--- client/src/components/Services/Services.js
+++ client/src/components/Services/Services.js
@@ -4,20 +4,20 @@ import css from './Services.css';
 import Service from '../Service/Service';
 import servicesData from './Services.data';
 
-export default class Services extends React.Component {
-  render() {
-    return (
-      <div className="services">
-        <h1 className="services-header">SERVICES</h1>
-        <div className="services-wrapper">
-          {servicesData.map((service, index) =>
-            <Service
-              service={service}
-              key={index}
-            />
-          )}
-        </div>
+const Services = props => {
+  return (
+    <div className="services">
+      <h1 className="services-header">SERVICES</h1>
+      <div className="services-wrapper">
+        {servicesData.map((service, index) =>
+          <Service
+            service={service}
+            key={index}
+          />
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Services;
