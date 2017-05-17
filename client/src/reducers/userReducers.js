8eadc1138d5af129120e65c5d9ae79143fc77830
Alacritas <jhwheeler@gmail.com>
2017-05-17 21:35:52 +0300

    Change answer to user reducer; add ADD_USER

---
+++ client/src/reducers/userReducers.js
@@ -0,0 +1,30 @@
+const initialState = {
+  answers: [],
+}
+
+const users = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_USER':
+      return [
+        ...state,
+        Object.assign({},
+        name: action.name,
+        email: action.email,
+        )
+      ];
+    case 'ADD_ANSWER':
+      return [...state, {
+        answer: action.answer,
+      }];
+    case 'UPDATE_ANSWER':
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1),
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
+export default users;
