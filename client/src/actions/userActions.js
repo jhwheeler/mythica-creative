1641d3f34601561025e97e3a0127b2a7b14e7c3d
Alacritas <jhwheeler@gmail.com>
2017-05-18 19:42:59 +0300

    Remove answer actions & fix parameters of addUser

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,27 +1,9 @@
 const ADD_USER = 'ADD_USER';
-const ADD_ANSWER = 'ADD_ANSWER';
-const UPDATE_ANSWER = 'UPDATE_ANSWER';
 
-export const addUser = (question, text)=> {
+export const addUser = (name, email) => {
   return {
     type: ADD_USER,
     name,
     email,
-  }
-}
-
-export const addAnswer = (question, text)=> {
-  return {
-    type: ADD_ANSWER,
-    question,
-    text,
-  }
-}
-
-export const updateAnswer = (question, answer) => {
-  return {
-    type: UPDATE_ANSWER,
-    question,
-    answer,
   }
 }
