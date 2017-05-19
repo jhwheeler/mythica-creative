2098f00849771618c4a8c658390d4667f0296075
Alacritas <jhwheeler@gmail.com>
2017-05-19 15:27:55 +0300

    Remove user action/reducer

--- client/src/actions/userActions.js
+++
@@ -1,9 +0,0 @@
-const ADD_USER = 'ADD_USER';
-
-export const addUser = (name, email) => {
-  return {
-    type: ADD_USER,
-    name,
-    email,
-  }
-}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
-import users from './userReducers';
 import advice from './adviceReducers';
 
 const reducers = {
-  users,
   advice,
   form: formReducer,
 };
