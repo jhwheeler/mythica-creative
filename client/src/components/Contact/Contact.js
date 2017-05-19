372b4d5cce366e2c06be088a1fcaa947c6f09791
Alacritas <jhwheeler@gmail.com>
2017-05-19 15:49:59 +0300

    Replace class/for with className/htmlFor

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -11,16 +11,16 @@ const Contact = () => {
     <div className="contact">
       <Hero className="contact-hero" backgroundImage={contactHero}>
         <div className="contact-form-wrapper">
-          <form class="contact-form">
+          <form className="contact-form">
             <fieldset name="contact-info">
-              <label for="name" className="visually-hidden">Your Name</label>
+              <label htmlFor="name" className="visually-hidden">Your Name</label>
               <br/>
               <input className="input" type="text" name="name" placeholder="Your Name" id="name" />
-              <label for="email" className="visually-hidden">Your Email</label>
+              <label htmlFor="email" className="visually-hidden">Your Email</label>
               <br/>
               <input className="input" type="email" name="email" placeholder="Your Email" id="email" />
               <br/>
-              <label for="message" className="visually-hidden">What's on your mind?</label>
+              <label htmlFor="message" className="visually-hidden">What's on your mind?</label>
               <br/>
               <textarea name="message" placeholder="What's on your mind?" id="message" />
               <Button className="contact-button" label="START" />
