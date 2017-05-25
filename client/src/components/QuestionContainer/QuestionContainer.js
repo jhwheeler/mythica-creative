a5e93a6bdeec4be4dd186196c8f8697246b8b148
Alacritas <jhwheeler@gmail.com>
2017-05-25 23:42:10 +0300

    Fix combined question view; remove multiple option

--- client/src/components/QuestionContainer/QuestionContainer.js
+++ client/src/components/QuestionContainer/QuestionContainer.js
@@ -10,9 +10,6 @@ import css from './QuestionContainer.css';
 export default class QuestionContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      currentSubQuestion: 0
-    }
     this.renderQuestions = this.renderQuestions.bind(this);
     this.updateSubQuestion = this.updateSubQuestion.bind(this);
   }
@@ -23,23 +20,17 @@ export default class QuestionContainer extends Component {
 
   renderQuestions(question) {
     if (question.component === "combined") {
-     return question.subQuestions.map((subQuestion, index) =>
-        <Question
-          question={subQuestion}
-        />
-     )} else if (question.component === "multiple") {
-       return question.subQuestions.map((subQuestion, index) => {
-         if (index <= this.state.currentSubQuestion) {
-           return (
-             <Question
-               question={question.subQuestions[index]}
-               key={index}
-               onBlur={this.updateSubQuestion}
-             />
-           )
-         }
-       })
-     } else {
+      return (
+       <div className="multi-question-wrapper">
+         <label>{question.text}</label>
+         {question.subQuestions.map((subQuestion, index) => (
+           <Question
+             question={subQuestion}
+             key={index}
+           />
+         ))}
+       </div>
+      )} else {
        return (
          <Question
            question={question}
