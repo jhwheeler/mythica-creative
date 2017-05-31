1514a0975e656a4c8a4b166ae2c9b25c6ad8ee2e
Alacritas <jhwheeler@gmail.com>
2017-05-31 22:16:39 +0300

    Create modals for services

--- client/src/components/Service/Service.js
+++ client/src/components/Service/Service.js
@@ -21,17 +21,40 @@ export default class Service extends Component {
   }
 
   render() {
-    const { icon, header, description } = this.props.service;
+    const {
+      icon,
+      header,
+      description,
+      modalTitle,
+      modalHeading1,
+      modalHeading2,
+      modalImage1,
+      modalImage2,
+      modalText1,
+      modalText2,
+      modalText3,
+      modalText4,
+    } = this.props.service;
     return (
       <div className="service">
-        <img src={icon} onClick={() => this.toggleModal()} />
+        <img className="service-icon" src={icon} onClick={() => this.toggleModal()} />
         <h3>{header}</h3>
-        <p>{description}</p>
+        <p className="service-description">{description}</p>
         <Button label="EXPLORE" onClick={() => this.toggleModal()} />
         <Modal isOpen={this.state.isModalOpen} onClose={() => this.toggleModal()}>
-          <h1>Modal title</h1>
-          <p>Hello</p>
-          <button onClick={() => this.toggleModal()}>Close</button>
+          <h1 className="service-modal-title">{modalTitle}</h1>
+          <div className="modal-section">
+            <img src={modalImage1} />
+            <h2>{modalHeading1}</h2>
+            <p>{modalText1}</p>
+            <p>{modalText2}</p>
+          </div>
+          <div className="modal-section">
+            <img src={modalImage2} />
+            <h2>{modalHeading2}</h2>
+            <p>{modalText3}</p>
+            <p>{modalText4}</p>
+          </div>
         </Modal>
       </div>
     );
--- client/src/components/Services/Services.data.js
+++ client/src/components/Services/Services.data.js
@@ -1,28 +1,72 @@
 import webDesignIcon from '../../../public/images/services/web_design_icon.png';
+import webDesignImage1 from '../../../public/images/services/web_design_1.jpg';
+import webDesignImage2 from '../../../public/images/services/web_design_2.jpg';
 import brandDesignIcon from '../../../public/images/services/brand_design_icon.png';
+import brandDesignImage1 from '../../../public/images/services/brand_design_1.jpg';
+import brandDesignImage2 from '../../../public/images/services/brand_design_2.jpg';
 import printDesignIcon from '../../../public/images/services/print_design_icon.png';
+import printDesignImage1 from '../../../public/images/services/print_design_1.jpg';
+import printDesignImage2 from '../../../public/images/services/print_design_2.jpg';
 import copyIcon from '../../../public/images/services/copy_icon.png';
+import copyDesignImage1 from '../../../public/images/services/copy_1.jpg';
+import copyDesignImage2 from '../../../public/images/services/copy_2.jpg';
 
 const servicesData = [
   {
     icon: webDesignIcon,
     header: 'WEB',
-    description: 'Using the latest technologies and trends, we provide sophisticated, responsive web design solutions that help you reach clients worldwide.'
+    description: 'Using the latest technologies and trends, we provide sophisticated, responsive web design solutions that help you reach clients worldwide.',
+    modalTitle: 'Web Design',
+    modalHeading1: 'EFFICIENT & ELEGANT',
+    modalHeading2: 'MOBILE-RESPONSIVE',
+    modalImage1: webDesignImage1,
+    modalImage2: webDesignImage2,
+    modalText1: 'We help you convert your visitors into clients.',
+    modalText2: 'Our web design approach is based on a deep understanding of the web environment, modern design tendencies, and contemporary marketing strategies.',
+    modalText3: 'Our experienced design team is committed to building websites that are both efficient and elegant to create the smoothest, most enjoyable experience for your audience.',
+    modalText4: 'In 2015, 52% of internet users were browsing on their mobile devices – now in 2016, that number has increased to 63%. This makes it essential to have a website that is mobile-responsive, adapting to whatever screen your potential client is using.',
   },
   {
     icon: brandDesignIcon,
     header: 'BRAND',
-    description: 'With over 10 years in the brand industry, we know exactly how to shape your corporate identity into an entrepreneurial legend.'
+    description: 'With over 10 years in the brand industry, we know exactly how to shape your corporate identity into an entrepreneurial legend.',
+    modalTitle: 'Brand Design',
+    modalHeading1: 'THE CORE OF YOUR SUCCESS',
+    modalHeading2: 'OUR DESIGN FORMULA',
+    modalImage1: brandDesignImage1,
+    modalImage2: brandDesignImage2,
+    modalText1: 'Your brand is more than just an image – it’s the core of your success.',
+    modalText2: 'It is the story expressing the essence of your services. It is what creates that initial blink reaction in the minds of your potential clients, determining their interest in your services. This is the first step to converting visitors into clients.',
+    modalText3: 'In our 10 years of experience, we have crafted the brand design formula to perfectly express your success in one elegant unity.',
+    modalText4: 'There is more than beauty and modern tendencies involved in developing a strong brand: what is needed is a deep understanding of consumer psychology, decision-making processes, and classic design principles expressed in a modern voice.',
   },
   {
     icon: printDesignIcon,
     header: 'PRINT',
-    description: 'Our attention to detail and stylistic precision give you the print materials to create the ideal impression of your services.'
+    description: 'Our attention to detail and stylistic precision give you the print materials to create the ideal impression of your services.',
+    modalTitle: 'Print Design',
+    modalHeading1: 'EFFECTIVE PRINT MATERIALS',
+    modalHeading2: 'HIGHEST QUALITY',
+    modalImage1: printDesignImage1,
+    modalImage2: printDesignImage2,
+    modalText1: 'At a conference, you hand your beautifully crafted business card to a prospective client. Their look of admiration says it all – you know they’ll be in touch.',
+    modalText2: 'No matter what your business, effective print materials are essential to establishing authority and trust – as well as reaching potential customers.',
+    modalText3: 'We are here to help you present your company to the world in the best possible way.',
+    modalText4: 'With a deep understanding of stylistic principles and a detail-oriented mindset, our team designs only the highest quality of products, from brochures and presentations to posters and billboards.',
   },
   {
     icon: copyIcon,
     header: 'COPY',
-    description: 'Our writers are here to understand your business in depth and clearly express it in a way your customers will understand.'
+    description: 'Our writers are here to understand your business in depth and clearly express it in a way your customers will understand.',
+    modalTitle: 'Copy',
+    modalHeading1: 'Engaging Copy',
+    modalHeading2: 'Express Your Success',
+    modalImage1: printDesignImage1,
+    modalImage2: printDesignImage2,
+    modalText1: 'At Mythica Creative, we believe in a holistic approach.',
+    modalText2: 'To effectively convert visitors into clients, it is necessary to communicate with your audience both visually and verbally, using copy that pulls the reader in and stimulates their imagination.',
+    modalText3: 'Connecting the visual design with the verbal message of your services, our linguistic experts craft the copy to express your success.',
+    modalText4: 'From brochures to billboards and books to blogs, we deliver valuable content to help you create that truly personal connection with your audience.',
   }
 ]
 
