f76795632983f271ddbf7fcff84acbb90a511e4e
Alacritas <jhwheeler@gmail.com>
2017-06-01 20:23:35 +0300

    Replace styled advice button with image

--- client/src/components/AdviceButton/AdviceButton.js
+++ client/src/components/AdviceButton/AdviceButton.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './AdviceButton.css';
+import adviceIcon from '../../../public/images/quiz/advice_icon.png';
 
-const AdviceButton = props => <div className="advice-button" onClick={props.onClick}></div>;
+const AdviceButton = props => {
+  return (
+    <img
+      className="advice-button"
+      onClick={props.onClick}
+      src={adviceIcon}
+    />
+  );
+}
 
 AdviceButton.PropTypes = {
   onClick: PropTypes.func,
