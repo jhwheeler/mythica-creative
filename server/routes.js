e78294f8a28404de3c157a37d33a113a01806676
Alacritas <jhwheeler@gmail.com>
2017-06-06 16:21:15 +0300

    Write get, put, and delete requests for answers api

--- server/routes.js
+++ server/routes.js
@@ -23,6 +23,19 @@ router.get('/answers', (req, res) => {
     );
 });
 
+router.get('/answers/:_id', (req, res) => {
+  Answer
+    .findOne({_id: req.params._id})
+    .exec()
+    .then(data => res.json(data))
+    .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal Server Error'})
+      }
+    );
+});
+
 router.post('/answers', (req, res) => {
   const date = new Date();
 
@@ -36,6 +49,40 @@ router.post('/answers', (req, res) => {
       console.error(err);
       res.status(500).json({message: 'Internal Server Error'});
     });
+});
+
+
+router.put('/answers/:_id', (req, res) => {
+  if (!(req.params._id && req.body._id && (req.params._id === req.body._id))) {
+    const message = (
+      `Request path id (${req.params._id}) and request body id ` +
+      `(${req.body._id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['answers', 'date']
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Answer
+    .findOneAndUpdate({_id: req.params._id}, {$set: toUpdate})
+    .exec()
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json(err));
+});
+
+router.delete('/answers/:_id', (req, res) => {
+  Answer
+    .findOneAndRemove({_id: req.params._id})
+    .exec()
+    .then(data => res.status(204).json(data))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 module.exports = {router}
