74961a7218236bd06a92012aa88db29471e19d01
Alacritas <jhwheeler@gmail.com>
2017-06-07 19:26:45 +0300

    Remove unused advice actions/reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
-import advice from './adviceReducers';
 import answers from './answerReducers';
 
 const reducers = {
-  advice,
   answers,
   form: formReducer,
 };
