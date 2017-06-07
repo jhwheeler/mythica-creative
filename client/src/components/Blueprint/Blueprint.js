38357a2393f001bd4c3a67256a33061ac072a07a
Alacritas <jhwheeler@gmail.com>
2017-06-07 15:01:29 +0300

    Add Blueprint component skeleton

---
+++ client/src/components/Blueprint/Blueprint.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Hero from '../Hero/Hero';
+import data from './Blueprint.data';
+
+const Blueprint = () => {
+  return (
+    <div className="blueprint">
+    </div>
+  );
+}
+
+export default Blueprint;
