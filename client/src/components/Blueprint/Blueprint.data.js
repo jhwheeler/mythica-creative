5431914ef8944e6cad5406351a883a210bfd8ffc
Alacritas <jhwheeler@gmail.com>
2017-06-08 07:14:59 +0300

    Copy & images for first three sections

---
+++ client/src/components/Blueprint/Blueprint.data.js
@@ -0,0 +1,17 @@
+import blueprintHero from '../../../public/images/hero/blueprint-hero.jpg';
+import mythicaLogo from '../../../public/images/hero/logo_hero.png';
+
+export const copy = {
+  heroTitle: "Your Brand Blueprint",
+  intro:"This is your Brand Blueprint, a tool you can use as a foundation to build your brand. We've combined the answers you gave us with some more in-depth information to help you along the way -- and in addition, you'll find exercises for you to deepen your knowledge. Let's get started!",
+  audienceTitle: "Know Your Audience",
+  audienceText1: "Yes, you've heard it time and time again, and you've already got it done and dusted. But, do you really? Go onto your social media right now and find dwhat your ideal clients search, discuss, and want. Open a text editor and paste some ideas in there. Spend a couple hours researching <em>exactly</em> what they're posting, tweeting, and liking. Paste in your file what your followers share on their walls and create a hot list of their biggest interests.",
+  audienceText2: "As brand experts, we work with buyer personas. Buyer personas are fictional, generalized representations of your ideal clients. They help you understand your clients (current and potential!) much better, and make it easier for you to tailor content to the specific needs, behaviors, and concerns of those different groups.",
+
+}
+
+export const images = {
+  blueprintHero,
+  mythicaLogo,
+
+}
