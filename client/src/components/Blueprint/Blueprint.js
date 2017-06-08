87f95032c4badda07b2ccb75ff8e42e658bf00c9
Alacritas <jhwheeler@gmail.com>
2017-06-08 07:13:57 +0300

    Structure first three sections

--- client/src/components/Blueprint/Blueprint.js
+++ client/src/components/Blueprint/Blueprint.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 
 import Hero from '../Hero/Hero';
-import data from './Blueprint.data';
+import { fetchAnswers } from '../../actions/answerActions';
 
-const Blueprint = () => {
-  return (
-    <div className="blueprint">
-    </div>
-  );
+import css from './Blueprint.css';
+
+import { copy, images } from './Blueprint.data.js';
+
+export default class Blueprint extends Component {
+  render() {
+    return (
+      <div className="blueprint">
+
+        <div className="blueprint-hero">
+          <Hero backgroundImage={images.blueprintHero}>
+            <div className="blueprint-hero-logo">
+              <img src={images.mythicaLogo} />
+            </div>
+            <div className="blueprint-hero-title">
+              <h1>{copy.heroTitle}</h1>
+            </div>
+          </Hero>
+        </div>
+
+        <div className="blueprint-intro">
+          <h2>Hi Jackson,</h2>
+          <p>{copy.intro}</p>
+        </div>
+
+        <div className="blueprint-audience">
+          <h2>{copy.audienceTitle}</h2>
+          <p>{copy.audienceText1}</p>
+          <p>{copy.audienceText2}</p>
+        </div>
+
+      </div>
+    );
+  }
 }
 
-export default Blueprint;
+const mapStateToProps = state => {
+  return {
+    answers: state.answers
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAnswers: answers => dispatch(fetchAnswers(answers)),
+  }
+}
