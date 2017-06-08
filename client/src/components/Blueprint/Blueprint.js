2e4fcb638ea0413990741afc9c76699f858a2f27
Alacritas <jhwheeler@gmail.com>
2017-06-08 10:46:40 +0300

    Connect to redux store; dispatch fetchAnswers in componentWillMount; buyerPersona skeleton

--- client/src/components/Blueprint/Blueprint.js
+++ client/src/components/Blueprint/Blueprint.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Hero from '../Hero/Hero';
 import { fetchAnswers } from '../../actions/answerActions';
 
 import css from './Blueprint.css';
-
 import { copy, images } from './Blueprint.data.js';
 
-export default class Blueprint extends Component {
+class Blueprint extends Component {
+
+  componentWillMount() {
+    let userId;
+
+    if (this.props.match && this.props.match.params) {
+      userId = this.props.match.params._id;
+    }
+
+    this.props.fetchAnswers(userId);
+  }
+
   render() {
+
+    const answers = this.props.answers;
+
     return (
       <div className="blueprint">
 
@@ -29,9 +43,32 @@ export default class Blueprint extends Component {
         </div>
 
         <div className="blueprint-audience">
-          <h2>{copy.audienceTitle}</h2>
-          <p>{copy.audienceText1}</p>
-          <p>{copy.audienceText2}</p>
+          <img src={images.audience} />
+          <div className="blueprint-audience-text">
+            <h2>{copy.audienceTitle}</h2>
+            <p>{copy.audienceText1}</p>
+            <p>{copy.audienceText2}</p>
+          </div>
+        </div>
+
+        <div className="buyer-persona">
+          <h2>{copy.buyerDemographicsTitle}</h2>
+          <div className="buyer-persona-wrapper">
+            <div className="buyer-persona-section buyer-persona-age">
+              <img src={images.audienceAgeYoung} />
+              <h3>Age group</h3>
+            </div>
+            <div className="buyer-persona-section buyer-persona-sex">
+              <img src={images.audienceSexMale} />
+              <h3>Sex</h3>
+            </div>
+            <div className="buyer-persona-section buyer-persona-income">
+              <p>$500,000</p>
+              <h3>Income</h3>
+            </div>
+            <div className="buyer-persona-description">
+            </div>
+          </div>
         </div>
 
       </div>
@@ -50,3 +87,5 @@ const mapDispatchToProps = dispatch => {
     fetchAnswers: answers => dispatch(fetchAnswers(answers)),
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Blueprint);
