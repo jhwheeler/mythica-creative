a45757adb5d13aabae4bc83053596725931aff7b
Alacritas <jhwheeler@gmail.com>
2017-06-09 00:47:42 +0300

    Remove images; add more text

--- client/src/components/Blueprint/Blueprint.data.js
+++ client/src/components/Blueprint/Blueprint.data.js
@@ -1,14 +1,3 @@
-import blueprintHero from '../../../public/images/hero/blueprint-hero.jpg';
-import mythicaLogo from '../../../public/images/hero/logo_hero.png';
-import audience from '../../../public/images/blueprint/audience.png';
-import audienceAgeYoung from '../../../public/images/quiz/audience-age-young.png';
-import audienceAgeMiddleAged from '../../../public/images/quiz/audience-age-middle-aged.png';
-import audienceAgeElderly from '../../../public/images/quiz/audience-age-elderly.png';
-import audienceSexMale from '../../../public/images/quiz/audience-sex-male.png';
-import audienceSexFemale from '../../../public/images/quiz/audience-sex-female.png';
-import audienceSexOther from '../../../public/images/quiz/audience-sex-other.png';
-import exercise from '../../../public/images/blueprint/exercise.png';
-
 export const copy = {
   heroTitle: "Your Brand Blueprint",
   intro:"This is your Brand Blueprint, a tool you can use as a foundation to build your brand. We've combined the answers you gave us with some more in-depth information to help you along the way -- and in addition, you'll find exercises for you to deepen your knowledge. Let's get started!",
@@ -17,23 +6,23 @@ export const copy = {
   audienceText2: "As brand experts, we work with buyer personas. Buyer personas are fictional, generalized representations of your ideal clients. They help you understand your clients (current and potential!) much better, and make it easier for you to tailor content to the specific needs, behaviors, and concerns of those different groups.",
   buyerDemographicsTitle: "Your Buyer Persona",
   buyerDescriptionTitle: "Who Are They?",
-  buyerDescriptionText: "This is sample text to simulate lorem ipsum lorem lorem ipse si adveniunt, quod vis facere? Non sunt multi qui volunt hoc facere, non est? Sic, ita est, sed quod vis? Sic est.",
+  solutionTitle: "How You Solve Their Challenge",
   buyerExerciseTitle: "Buyer Persona Exercise",
   buyerExerciseText1: "Create at least two buyer personas using the ready-to-go, fill-in-the-blank worksheets we sent you. They don't have to include all the details from the get go; you can flesh them out over time. In the beginning, fill out at least the name, age, main interests, pain points, and desires. As for the name, pick a typical name for the demographic, just to make them more real, personal, and relatable.",
   buyerExerciseText2: "This will help you when you write your blog posts, shoot videos, or create any other kind of content. Knowing who you're speaking to will give you a clear direction and focus that will exponentially improve your results in all your marketing efforts. Remember: the client is the most important aspect of your entire enterprise!",
+  missionVisionTitle: "Mission and Vision",
+  missionVisionText1: "Now that you know your strongest characteristics, let's focus on your brand message. This is the ideology you stand behind, your personal belief system that differentiates you from your competitors. A real hero stands behind a true mission and vision.",
+  missionVisionText2: "Your vision is the future world you see in your mind -- what you're fighting for, what you're striving to create. This is the ultimate end state you know in your heart of hearts is possible -- and you won't stop until you make it real.",
+  missionVisionText3: "Your mission is built from the concrete action steps you will take to manifest that vision. How will you make your vision a reality? What are the stages of that work? Plan out each of those stages and the specific steps needed to get from one stage to the next.",
+  visionTitle: "Your Vision",
+  missionTitle: "Your Mission",
+  visionExerciseTitle: "Vision Exercise",
+  visionExerciseText1: "Now we're going to dive deeper into your vision. Prepare your favorite drink and take a minute to relax. Get comfortable in your seat and settle down. This is a time just for you.",
+  visionExerciseText2: "Take a deep breath, close your eyes, and ask yourself: 'What do I truly want? What world do I want to live in?' Let your mind's eye w ander and explore the terrain: what do you see around you? Where are you? Who is around you? What are they doing and how do they feel? How do you feel? Imagine your ideal world and just exlplore it...",
+  visionExerciseText3: "Now write down how your business plays a role in creating this world. Be brave and dream big -- there are no limits to your vision.",
+  visionExerciseText4: "If you're up for it, make a visionboard: gather images that illustrate your vision and paste them into a file. Then print it out and post it up somewhere in your office or home as a constant reminder of the why behind your work.",
+  visualStyleTitle: "Your Visual Message",
+  visualStyleText: "Humans are in large part visual creatures. Communicating with your audience in an authentic way begins with that first impression; this is why the visual message you express is key to a strong connection.",
 
-}
-
-export const images = {
-  blueprintHero,
-  mythicaLogo,
-  audience,
-  audienceAgeYoung,
-  audienceAgeMiddleAged,
-  audienceAgeElderly,
-  audienceSexMale,
-  audienceSexFemale,
-  audienceSexOther,
-  exercise,
 
 }
